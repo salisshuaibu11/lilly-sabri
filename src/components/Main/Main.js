d04f0e2192c060915d6524626e3376a262592822
salis <salisshuaibu11@gmail.com>
2021-09-13 01:19:13 +0000

    Implemented sign up now button

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { UserCircleIcon } from "@heroicons/react/outline";
+import "./Main.css";
 
 const Main = () => {
   return (
@@ -9,6 +10,17 @@ const Main = () => {
           <UserCircleIcon width={25} />{" "}
           <span>THE LEAN APP IS COMING GUIDE!</span>
         </div>
+        <a
+          target="__blank"
+          href="https://mailchi.mp/leanwithlilly/freeworkouttimetable"
+        >
+          sign up now for free guides!
+          <UserCircleIcon
+            className="clone-icon"
+            width={25}
+            style={{ marginLeft: "5rem" }}
+          />
+        </a>
       </article>
       <div className="main__bottom">main__bottom</div>
     </main>
