e64a29be3e40b4e37edd4a17a3fa1fdd6c7e43b1
salis <salisshuaibu11@gmail.com>
2021-09-13 11:36:26 +0000

    Added dummy data

---
+++ src/data.js
@@ -0,0 +1,29 @@
+export const data = [
+  {
+    id: 0,
+    name: "Get my Cookbook!",
+    description: "BUY NOW",
+    url:
+      "https://www.leanwithlilly.com/products/the-anti-inflammatory-cook-book/",
+  },
+  {
+    id: 1,
+    name: "SHOP GYMSHARK using my link!",
+    description: "BUY GORGEOUS CLOTHING NOW",
+    url:
+      "https://uk.gymshark.com/collections/new-releases/womens?utm_source=ACA&utm_medium=3728830_Lilly+Sabri&utm_campaign=instagram&ranMID=45837&ranEAID=8hecCNwSw7c&ranSiteID=8hecCNwSw7c-vPY6T6bsAnScCgL8_CVKFw",
+  },
+  {
+    id: 2,
+    name: "50% OFF my best selling 8 WEEK LEAN TRANSFORMATION GUIDE",
+    description: "BUY NOW",
+    url:
+      "https://www.leanwithlilly.com/products/video-8-week-lean-transformation-guide",
+  },
+  {
+    id: 3,
+    name: "LEAN RESISTANCE BANDS",
+    description: "SHOP NOW!",
+    ulr: "https://www.leanwithlilly.com/collections/resistance-bands/",
+  },
+];
